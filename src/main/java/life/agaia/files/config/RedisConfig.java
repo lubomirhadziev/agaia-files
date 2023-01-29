bf8f5b05656c24a7be494a26c2405f0c683dcd0c
Lyubomir Hadzhiev <lubomir.hadziev@gmail.com>
2023-01-29 18:00:53 +0200

    more cache

--- src/main/java/life/agaia/files/config/RedisConfig.java
+++ src/main/java/life/agaia/files/config/RedisConfig.java
@@ -13,7 +13,7 @@ public class RedisConfig {
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
-            .entryTtl(Duration.ofDays(3))
+            .entryTtl(Duration.ofDays(60))
             .disableCachingNullValues()
             .prefixCacheNameWith("agaia-files-")
             .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
